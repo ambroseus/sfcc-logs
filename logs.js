5f9322d167089aae885478220fa4892ebff92d20
eugene.samonenko <eugene.samonenko@valtech.com>
2018-01-25 18:13:58 +0200

    prettify

--- logs.js
+++ logs.js
@@ -2,24 +2,27 @@
 
 const fs = require('fs');
 const path = require('path');
-const options = require('./config.json');
+const config = require('./config.json');
 const { analyzeLogs } = require('./analyzer.js');
 const { summaryHtml } = require('./formatter.js');
 
-const date = process.argv[2] || '';
+const date = process.argv[2] || ''; // YYYYMMDD
 
-const logs = fs.readdirSync(options.LOGSDIR)
-		.filter( file => path.extname(file) === '.log' && (!date || file.indexOf(date) > 0) )
-		.map( file => path.join(options.LOGSDIR, file) );
+const logs = fs.readdirSync(config.LOGSDIR)
+		.filter( fname => path.extname(fname) === '.log' && (!date || fname.indexOf(date) > 0) )
+		.map( fname => path.join( config.LOGSDIR, fname ) );
 
 const errors = analyzeLogs(logs);
-const topErrors = Object.keys(errors)
-		.filter( key => errors[key].total > options.MAXERRORS )
-		.sort( (a,b) => errors[b].total - errors[a].total )
-		.map(key => errors[key]);
 
-const file = path.join(options.LOGSDIR, `${options.SUMMARY}-${date}.html`);
-console.log(`output: ${file}`);
-fs.writeFileSync( file, summaryHtml(topErrors) );
+const topErrors = Object.keys(errors)
+		.filter( key => errors[key].total > config.MAXERRORS )
+		.sort( (a,b) => errors[b].total - errors[a].total )
+		.map( key => errors[key] );
+
+const summary = path.join( config.LOGSDIR, `${config.SUMMARY}${date}.html` );
+
+console.log(`output: ${summary}`);
+fs.writeFileSync( summary, summaryHtml(topErrors) );
+
 console.log('done.');
 
